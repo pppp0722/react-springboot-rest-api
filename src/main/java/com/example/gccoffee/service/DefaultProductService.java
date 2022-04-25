ac05eb3c74fe877099e7c19d68ab9da29e8afdc4
pppp0722 <poj0722@naver.com>
2022-04-25 20:07:05 +0900

    [add] product CRUD

---
+++ src/main/java/com/example/gccoffee/service/DefaultProductService.java
@@ -0,0 +1,42 @@
+package com.example.gccoffee.service;
+
+import com.example.gccoffee.model.Category;
+import com.example.gccoffee.model.Product;
+import com.example.gccoffee.repository.ProductRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DefaultProductService implements ProductService{
+
+    ProductRepository repository;
+
+    public DefaultProductService(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<Product> getProductsByCategory(Category category) {
+        return repository.findByCategory(category);
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Product createProduct(String productName, Category category, long price) {
+        var product = new Product(UUID.randomUUID(), productName, category, price);
+        return repository.insert(product);
+    }
+
+    @Override
+    public Product createProduct(String productName, Category category, long price,
+        String description) {
+        var product = new Product(UUID.randomUUID(), productName, category, price, description, LocalDateTime.now(), LocalDateTime.now());
+        return repository.insert(product);
+    }
+}
